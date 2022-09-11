1d2092bbeebe34f548d0c63f302857944b51af33
paul lovato <lovato.paul97@yahoo.com>
2022-09-11 11:44:10 -0400

    Questions generated and output writefile complete

--- index.js
+++ index.js
@@ -1,26 +1,67 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+const path = require('path');
 const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
 const { stringify } = require('querystring');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'projectName',
+        name: 'title',
         message: 'What is your project name?'
     },
     {
         type: 'input',
+        name: 'github',
+        message: 'What is your github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what is your email address?'
+    },
+    {
+        type: 'input',
         name: 'description',
-        message: 'Describe your project.'
+        message: 'please write a short description of your project'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies?'
+    },
+    {
+        type: 'input',
+        name: 'command',
+        message: 'What command should be run to run tests?'
+    },
+    {
+        type: 'input',
+        name: 'using',
+        message: 'What does the user need to know about using the repo?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?'
     }
 ];
 
+const template = 
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+
     fs.writeFile(fileName, data, err => err ? console.log(err) : console.log('Readme Generated!'));
-}
+};
 
 
 // TODO: Create a function to initialize app
@@ -29,10 +70,7 @@ function init() {
     .prompt(questions)
     .then(answers => {
 
-    const answersArr = (JSON.stringify(answers));
-
-
-        writeToFile('README.md', answersArr);
+        writeToFile('output.json', JSON.stringify(answers))
     });
 };
 
